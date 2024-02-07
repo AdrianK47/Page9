b0c098ab602f5ceb1482a3af358260471c37ad67
AdrianK47 <kangadrian76@gmail.com>
2024-02-07 14:35:06 -0500

    what the flerm
    totsugeki

---
+++ Page9.java
@@ -0,0 +1,14 @@
+/**
+ * Progam Name: Page9
+ * Name: Adrian Kang
+ * Date: 2/7/2024
+ */
+public class Page9 {
+     public static void main (String[] args){
+        System.out.println("Hi"); //Message to user
+        System.out.println("LabOneA"); //Assignment
+    }
+}
+/* if you want to write a detailed comment, and it wraps
+around the screen, use this block style of commenting.
+*/
